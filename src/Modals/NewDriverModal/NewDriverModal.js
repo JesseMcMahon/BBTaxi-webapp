d7967319dd09ca93faabcc13df59868f009c75e2
Jesse McMahon <Jessemcmahon17@gmail.com>
2021-04-23 11:33:59 -0400

    update new driver file

--- src/Modals/NewDriverModal/NewDriverModal.js
+++ src/Modals/NewDriverModal/NewDriverModal.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react"
 import {AiOutlineClose} from "react-icons/ai"
-import Axios from "axios"
 import "./NewDriverModal.css"
+import Axios from 'axios'
 
-const NewDriverModal = (props) => {
+const NewDriverModal = () => {
 
-    const [driverFirstName, setDriverFirstName] = useState('')
-    const [driverLastName, setDriverLastName] = useState('')
-    const [driverPhone, setDriverPhone] = useState('')
-    const [driverEmail, setDriverFirstEmail] = useState('')
-    const [driverBirthdate, setDriverBirthdate] = useState('')
-    const [driverLicenseNumber, setDriverLicenseNumber] = useState('')
-    const [driverLicenseExp, setDriverLicenseExp] = useState('')
-    
+    const [phoneNum, setPhoneNum] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [email, setEmail] = useState('')
+    const [birthdate, setBirthdate] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
+    const [licenseNumber, setLicenseNumber] = useState('')
+    const [expirationDate, setExpirationDate] = useState('')
+
     const closeNewDriverModal = () => {
         const newDriverModal = document.getElementById("new-driver")
         newDriverModal.classList.remove("display-block")
@@ -20,31 +22,10 @@ const NewDriverModal = (props) => {
 
     const sendNewDriverRequest = (e) => {
         e.preventDefault()
-
-            const newDriver = {
-                driverFirstName,
-                driverLastName,
-                driverPhone,
-                driverEmail,
-                driverBirthdate,
-                driverLicenseNumber,
-                driverLicenseExp
-            }
-            Axios.post('http://localhost:3001/api/v1/newDriver', {newDriver})
-            .then(resp => {
-              if (resp.data.success) {
-                localStorage.setItem("token", resp.data.jwt)
-                props.handleLogin(resp.data.newDriver)
-                // redirect()
-              }
-              else {
-                console.log(resp.data.failure)
-              }
-             
-            })
-          
-        
-        console.log(driverFirstName, driverLastName, driverPhone, driverEmail, driverBirthdate,driverLicenseNumber, driverLicenseExp)
+        const driver = {first_name: firstName, last_name: lastName, phone_number: phoneNum, email, birthdate, password, password_confirmation: passwordConfirmation, license_number: licenseNumber, expiration_date: expirationDate}
+        console.log(driver)
+        Axios.post('http://localhost:3001/api/v1/drivers', driver)
+        .then(resp => console.log(resp))
     }
 
     return (
@@ -54,13 +35,15 @@ const NewDriverModal = (props) => {
         <div className="new-driver-modal-content">
         <AiOutlineClose onClick={closeNewDriverModal} className="close-modal" />
             <h1>Enter new driver Here</h1>
-            <input className="new-driver-input" type="text" placeholder="Driver First Name" onChange={(e) => setDriverFirstName(e.target.value)} />
-            <input className="new-driver-input" type="text" placeholder="Driver Last Name" onChange={(e) => setDriverLastName(e.target.value)} />
-            <input className="new-driver-input" type="text" placeholder="Driver Phone" onChange={(e) => setDriverPhone(e.target.value)} />
-            <input className="new-driver-input" type="text" placeholder="Driver Email" onChange={(e) => setDriverFirstEmail(e.target.value)} />
-            <input className="new-driver-input" type="text" placeholder="Driver Birthdate" onChange={(e) => setDriverBirthdate(e.target.value)} />
-            <input className="new-driver-input" type="text" placeholder="Driver License #" onChange={(e) => setDriverLicenseNumber(e.target.value)} />
-            <input className="new-driver-input" type="text" placeholder="Driver License Exp." onChange={(e) => setDriverLicenseExp(e.target.value)} />
+            <input className="new-driver-input" type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} />
+            <input className="new-driver-input" type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
+            <input className="new-driver-input" type="text" placeholder="Driver Phone" onChange={(e) => setPhoneNum(e.target.value)} />
+            <input className="new-driver-input" type="text" placeholder="Driver Email" onChange={(e) => setEmail(e.target.value)} />
+            <input className="new-driver-input" type="text" placeholder="Driver Birthdate" onChange={(e) => setBirthdate(e.target.value)} />
+            <input className="new-driver-input" type="text" placeholder="Driver License #" onChange={(e) => setLicenseNumber(e.target.value)} />
+            <input className="new-driver-input" type="text" placeholder="License Expiration Date" onChange={(e) => setExpirationDate(e.target.value)} />
+            <input className="new-driver-input" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <input className="new-driver-input" type="password" placeholder="Confirm Password" onChange={(e) => setPasswordConfirmation(e.target.value)} />
             <input className="create-driver-btn" type="submit" value="Create Driver" onClick={(e) => sendNewDriverRequest(e)} />
         </div>
     </div>
